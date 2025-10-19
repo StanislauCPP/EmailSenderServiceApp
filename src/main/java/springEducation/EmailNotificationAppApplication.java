5b04cfd9d61b5ce4eb8c2ec0492479ded3eda7e7
Stas <stanislaukasach@gmail.com>
2025-10-19 14:33:02 +0300

    pom.sml was rewritten for spring. EurekaClient was added. Big part of properties was replaced to config server

--- src/main/java/springEducation/EmailNotificationAppApplication.java
+++ src/main/java/springEducation/EmailNotificationAppApplication.java
@@ -2,8 +2,10 @@ package springEducation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EmailNotificationAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmailNotificationAppApplication.class, args);
